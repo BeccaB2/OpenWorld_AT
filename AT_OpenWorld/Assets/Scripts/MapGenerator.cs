1375f7859f2096c973fee5043bb2dd2f5b38b6f5
Becca <Rebecca2.Bradburn@live.uwe.ac.uk>
2020-01-31 15:46:25 +0000

    Noise map progress

--- AT_OpenWorld/Assets/Scripts/MapGenerator.cs
+++ AT_OpenWorld/Assets/Scripts/MapGenerator.cs
@@ -8,11 +8,15 @@ public class MapGenerator : MonoBehaviour
     public int mapHeight;
     public float noiseScale;
 
+    public int octaves;
+    public float persistance;
+    public float lacunarity;
+
     public bool autoUpdate;
     public void GenerateMap()
     {
         // Fetch the noise map from noise class
-        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, noiseScale);
+        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, noiseScale, octaves, persistance, lacunarity);
 
         // Calling the display map functionality & drawing it onto the plane
         DisplayMap display = FindObjectOfType<DisplayMap>();
